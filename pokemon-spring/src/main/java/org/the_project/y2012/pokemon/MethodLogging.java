e4f4f9207737d07526002d20d9c6d56916826264
Larry Mulcahy <lmulcahy@nyx.net>
2013-10-15 12:18:26 -0600

    Method logging with AOP

---
+++ pokemon-spring/src/main/java/org/the_project/y2012/pokemon/MethodLogging.java
@@ -0,0 +1,37 @@
+package org.the_project.y2012.pokemon;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MethodLogging {
+
+  @Around("execution(* org.the_project.y2012.pokemon..*.*(..))")
+  public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+
+      String signature = joinPoint.getSignature().getName();
+      String methodName;
+
+      if (joinPoint.getTarget() == null) {
+//          System.out.println("joinPoint.getTarget() == null");
+          methodName = signature + "()";
+      } else {
+//          System.out.println("joinPoint.getTarget() != null");
+          String className = joinPoint.getTarget().getClass().getName();
+          methodName = className + "." + signature + "()";
+      }
+
+      Logger.getLogger(this.getClass())
+              .info("Entering " + methodName + "...");
+
+      Object result = joinPoint.proceed();
+
+      Logger.getLogger(this.getClass()).info("Leaving " + methodName + ".");
+
+      return result;
+  }
+}
