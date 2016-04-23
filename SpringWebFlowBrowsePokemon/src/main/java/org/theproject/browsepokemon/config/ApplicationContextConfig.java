188ab8640d21c22f3dfc768457a3296f61db0e94
Larry Mulcahy <lmulcahy@nyx.net>
2016-04-23 14:16:42 -0600

    Use the @Value annotation to get property values, instead of explicitly using Environment.  And clean up some log outputs.

--- SpringWebFlowBrowsePokemon/src/main/java/org/theproject/browsepokemon/config/ApplicationContextConfig.java
+++ SpringWebFlowBrowsePokemon/src/main/java/org/theproject/browsepokemon/config/ApplicationContextConfig.java
@@ -6,13 +6,13 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.orm.hibernate3.HibernateTransactionManager;
 import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
@@ -30,8 +30,22 @@ public class ApplicationContextConfig {
     
     private static transient Logger logger = LoggerFactory.getLogger(ApplicationContextConfig.class);
 
-    @Autowired
-    private Environment env;
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+    propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    
+    @Value("${hibernate.connection.driver_class}") 
+    private String dbDriver;
+    @Value("${hibernate.connection.url}") 
+    private String dbUrl;
+    @Value("${hibernate.connection.username}") 
+    private String dbUser;
+    @Value("${hibernate.connection.password}") 
+    private String dbPassword;
+    @Value("${hibernate.dialect}")
+    private String dbDialect;
     
     // Do exception translation for components annotated @Repository
     @Bean
@@ -56,26 +70,23 @@ public class ApplicationContextConfig {
     // Session factory
     @Bean
     public AnnotationSessionFactoryBean getSessionFactory() {
-        logger.info("*** In getSessionFactory...");
         AnnotationSessionFactoryBean result = new AnnotationSessionFactoryBean();
         result.setDataSource(pokedexDataSource());
         result.setPackagesToScan("pokedex");
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("dialect", env.getProperty("hibernate.dialect"));
+        hibernateProperties.setProperty("dialect", dbDialect);
         result.setHibernateProperties(hibernateProperties);
-        logger.info("*** getSessionFactory: result = " + result);
         return result;
     }
     
     // Data source
     @Bean
     public BasicDataSource pokedexDataSource() {
-        logger.info("*** In pokedexDataSource...");
         BasicDataSource result = new BasicDataSource();
-        result.setDriverClassName(env.getProperty("hibernate.connection.driver_class"));
-        result.setUrl(env.getProperty("hibernate.connection.url"));
-        result.setUsername(env.getProperty("hibernate.connection.username"));
-        result.setPassword(env.getProperty("hibernate.connection.password"));
+        result.setDriverClassName(dbDriver);
+        result.setUrl(dbUrl);
+        result.setUsername(dbUser);
+        result.setPassword(dbPassword);
         result.setInitialSize(5);
         result.setMaxActive(10);
         return result;
@@ -84,10 +95,8 @@ public class ApplicationContextConfig {
     // Transaction manager
     @Bean
     public HibernateTransactionManager pokedexTransactionManager() {
-        logger.info("*** In pokedexTransactionManager...");
         HibernateTransactionManager result = new HibernateTransactionManager();
         SessionFactory sessionFactory = getSessionFactory().getObject();
-        logger.info("pokedexTransactionManager: sessionFactory = " + sessionFactory);
         result.setSessionFactory(sessionFactory);
         return result;
     }
