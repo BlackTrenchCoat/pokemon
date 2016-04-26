fa305aebb730686bf0c523b75dfaa6708f975eab
Larry Mulcahy <lmulcahy@nyx.net>
2016-04-26 05:01:07 -0600

    Clean up

--- SpringWebFlowBrowsePokemon/src/main/java/org/theproject/browsepokemon/config/PokeWebFlowAppInitializer.java
+++ SpringWebFlowBrowsePokemon/src/main/java/org/theproject/browsepokemon/config/PokeWebFlowAppInitializer.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import org.theproject.browsepokemon.model.PokedexDAOImpl;
 
 public class PokeWebFlowAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     
@@ -15,37 +14,24 @@ public class PokeWebFlowAppInitializer extends AbstractAnnotationConfigDispatche
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        
-        logger.info("*** In onStartup...");
-        
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(ApplicationContextConfig.class);
-        
-        // TODO Auto-generated method stub
+
         super.onStartup(servletContext);
     }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        
-        logger.info("*** In getRootConfigClasses...");
-        
         return new Class<?>[]{ ApplicationContextConfig.class };        
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        
-        logger.info("*** In getServletConfigClasses...");
-        
         return new Class<?>[]{ WebConfig.class };        
     }
 
     @Override
     protected String[] getServletMappings() {
-        
-        logger.info("*** In getServletMappings...");
-        
         return new String[] { "/flows/*" };        
     }
 
